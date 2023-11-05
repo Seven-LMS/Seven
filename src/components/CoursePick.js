79a1d7e70587090510d806bd63ef29d94c8a48d5
LAPTOP-DBRSJ9V5\User <virgieangel05@gmail.com>
2023-11-05 20:12:03 +0700

    Added fetching course list functionality

--- src/components/CoursePick.js
+++ src/components/CoursePick.js
@@ -1,34 +1,56 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+
+const listItemStyle = {
+    display: "flex",
+    justifyContent: "left",
+    alignItems: "center",
+    marginBottom: "5px",
+    padding: "5px",
+    fontSize: "10px",
+    textAlign: "left",
+};
+
+const iconStyle = {
+    marginRight: "10px",
+    fontSize: "12px",
+};
+
+const titleStyle = {
+    textAlign: "left", 
+};
 
 function CoursePick() {
     const [data, setData] = useState([]);
+
     useEffect(() => {
-        axios.get('http://localhost:3005/')
-        .then(res=>setData(res.data))
-        .catch(err=>console.log(err))
-    })
+        axios.get('http://localhost:3005/getCourses')
+            .then(res => setData(res.data))
+            .catch(err => console.log(err))
+    }, []);
 
     return (
-        <div className="content"> {/* Use className for defining CSS classes in JSX */}
-        <h2>Ini Course Pick</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>cid</th>
-                    <th>course name</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((d, i) => (
-                    <tr key={i}> {/*It's a good practice to add a unique 'key' prop when mapping */}
-                        <td>{d.cid}</td>
-                        <td>{d.name}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    </div>
+        <div className="content">
+            <div className="title" style={titleStyle}>
+                <h3>Courses</h3>
+            </div>
+            <div className="allcourse">
+                <ul className="allcourselist">
+                    {data.map((course, index) => (
+                        <li className="class1" key={index} style={listItemStyle}>
+                            <i className="fa-solid fa-square-check" style={iconStyle}><FontAwesomeIcon icon={faSquareCheck}/></i>
+                            <div className="classinfo">
+                                <h3>{course.name}</h3>
+                                <p>{`${course.semester}/${course.year}`}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     );
 }
 
