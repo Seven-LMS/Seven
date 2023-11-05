79611ec68e32898e54b10cd561173b0902e6ec51
LAPTOP-DBRSJ9V5\User <virgieangel05@gmail.com>
2023-11-06 02:01:44 +0700

    Added fetching course data in course pages functionality

--- src/components/ClassContent.js
+++ src/components/ClassContent.js
@@ -1,11 +1,34 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareCheck, faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function ClassContent() {
+function ClassContent({classId}) {
+    const [announcement, setAnnouncement] = useState([]);
+    const [clas, setClass] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3005/getAnnouncement/'+classId)
+            .then(res => {
+                console.log(res.data)
+                setAnnouncement(res.data)})
+            .catch(err => console.log(err));
+    }, []);
+
+    useEffect(() => {
+        axios.get('http://localhost:3005/getClass/'+classId)
+            .then(res => {
+                console.log(res.data)
+                setClass(res.data)})
+            .catch(err => console.log(err));
+    }, []);
+
     return (
         <div class="content">
-            <div class="classname">Software Engineering</div>
+            {clas.length > 0 ? (
+                <div className="classname">{clas[0].name}</div>
+                ) : null}
                 <div class="class-announcement">
                     <div class="head"><h3>Announcements</h3></div>
                         <ul class="classannlist">
--- src/page/ClassPage.js
+++ src/page/ClassPage.js
@@ -3,12 +3,17 @@ import Sidebar from '../components/Sidebar';
 import Header from '../components/Header'
 import ClassContent from '../components/ClassContent';
 
+const searchParams = new URLSearchParams(window.location.search);
+const classId = searchParams.get('classId');
+
 function ClassPage(){
+    console.log(classId)
     return(
         <div className="App">
             <Header/>
             <Sidebar/>
-            <ClassContent/>
+            <ClassContent
+            classId={classId}/>
         </div>
     );
 }
